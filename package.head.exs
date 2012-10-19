bead765a1a38f1a49c107068ea12682d27ad3346
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-19 12:47:48 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "exconfig", description: "Elixir configuration library",
+                 version: :head, keywords: ["config"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/exconfig"]])
+    
