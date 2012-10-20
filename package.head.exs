be777b80640b626f31d5b12fa2057413d0951a13
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-20 02:06:35 -0700

    Minor expm package update (more keywords)

--- package.head.exs
+++ package.head.exs
@@ -1,5 +1,5 @@
 Expm.Package.new(name: "exconfig", description: "Elixir configuration library",
-                 version: :head, keywords: ["config"], 
+                 version: :head, keywords: ["config", "ini", "setting"], 
                  maintainers: [[name: "Yurii Rashkovskii", 
                                 email: "yrashk@gmail.com"]],
                  repositories: [[github: "yrashk/exconfig"]])
